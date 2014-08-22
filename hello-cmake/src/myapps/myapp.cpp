03be484492b6c3f6ba2beeb00e31576776087b03
0000-bigtree <0000.bigtree@gmail.com>
2014-08-22 14:29:32 +0800

    learn cmake

---
+++ hello-cmake/src/myapps/myapp.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "mylib.h"
+
+int main(int argc, const char *argv[])
+{
+ MyLib.DoSomething();
+ return 0;
+}
---
+++ hello-cmake/src/mylibs/mylib.h
@@ -0,0 +1,12 @@
+// mylib.h
+//----------------------------------------------------------------------------------------------------
+#ifndef _MYLIB_H
+#define _MYLIB_H
+
+class MyLib{
+public:
+ void DoSomething();
+};
+
+#endif
+
